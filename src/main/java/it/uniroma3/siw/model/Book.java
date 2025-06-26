b3f22edc88c06894dec5df89dd52b50113748445
Leonardo Fabiani <fabianileonardo@outlook.com>
2025-06-26 09:16:37 +0200

    Add Book and Author models, enable validation
    Introduced the Book and Author model classes. Book is annotated as a JPA entity with validation constraints and relationships. Added the spring-boot-starter-validation dependency to pom.xml to support validation annotations.

---
+++ src/main/java/it/uniroma3/siw/model/Book.java
@@ -0,0 +1,71 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+public class Book {
+	@Id													//permette di definire questo attributo come chiave
+	@GeneratedValue(strategy = GenerationType.AUTO)		//permette di generare automaticamente l'id dell'oggetto
+	private Long id;
+	
+	@NotBlank											//permette di inserire una costrizione sul tipo di valore inserito in questo attributo, in questo caso si richiede che l'attributo non sia vuoto o con caratteri spazio
+	private String title;
+	
+	@Min(-3000)											//permettono di inserire un massimo ed un minimo valore a questo intero
+	@Max(2050)
+	private Integer publicationYear;
+	
+	@ElementCollection
+	private List<String> imagePaths; // Percorsi relativi a file salvati
+
+	@ManyToMany
+	private Set<Author> authors;
+	
+	
+	
+	
+	public List<String> getImagePaths() {
+		return imagePaths;
+	}
+
+	public void setImagePaths(List<String> imagePaths) {
+		this.imagePaths = imagePaths;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(Integer publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	
+}
